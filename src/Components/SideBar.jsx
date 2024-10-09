2e216646f61f5ef6e2528c42ab42180cea95e1eb
Ingeborg Brommeland Austeid <ingeborg_austeid@hotmail.com>
2024-10-09 11:15:09 +0200

    ..

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -2,20 +2,18 @@ import { Box } from "@mui/material";
 import "../App.css";
 import HomeIcon from "../assets/home-icon.svg";
 import ProfileIcon from "../assets/profile-icon.svg";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 function SideBar() {
   return (
     <aside>
       <Box className="sidebar-item">
-        
-          <Link to="/">
+        <Link to="/">
           <div>
-          <img src={HomeIcon} alt="home icon"/>
-          <h4 className="font-colour">Home</h4>
+            <img src={HomeIcon} alt="home icon" />
+            <h4 className="font-colour">Home</h4>
           </div>
-          </Link>
-        
+        </Link>
       </Box>
 
       <Box className="sidebar-item">
